ad4865cf0c6cd344d8f9ef25e8b923f547ec922a
jhstavares <jhstavares_01@hotmail.com>
2022-10-06 22:55:01 -0300

    teste git

---
+++ src/main/java/com/example/gerar/GerarApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerar/GerarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
